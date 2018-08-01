08d7681b9e124455f66025df87a01640bd3a6230
mhjhamza <muhammadhamzaleo@gmail.com>
2018-08-01 17:01:15 +0500

    Day 23 | Deep Learning with Keras

---
+++ Code/Day23/image_recognition final.py
@@ -0,0 +1,30 @@
+import numpy as np
+from keras.preprocessing import image
+from keras.applications import resnet50
+
+# Load Keras' ResNet50 model that was pre-trained against the ImageNet database
+model = resnet50.ResNet50()
+
+# Load the image file, resizing it to 224x224 pixels (required by this model)
+img = image.load_img("bay.jpg", target_size=(224, 224))
+
+# Convert the image to a numpy array
+x = image.img_to_array(img)
+
+# Add a forth dimension since Keras expects a list of images
+x = np.expand_dims(x, axis=0)
+
+# Scale the input image to the range used in the trained network
+x = resnet50.preprocess_input(x)
+
+# Run the image through the deep neural network to make a prediction
+predictions = model.predict(x)
+
+# Look up the names of the predicted classes. Index zero is the results for the first image.
+predicted_classes = resnet50.decode_predictions(predictions, top=9)
+
+print("This is an image of:")
+
+for imagenet_id, name, likelihood in predicted_classes[0]:
+    print(" - {}: {:2f} likelihood".format(name, likelihood))
+
